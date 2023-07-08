93a4108f121952efbc76e11460470ade6dd0f238
dihjital <peter.hrobar@gmail.com>
2023-07-08 09:26:20 +0200

    Bug fixes and performance improvements

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -39,7 +39,7 @@
                     </div>
                     <!-- Second row //-->
                     <div class="p-4 w-full">
-                        <a href="/questions/{{ $results?->highest_vote['id'] }}/votes" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                        <a href="{{ $results?->highest_vote['id'] ? '/questions/'.$results?->highest_vote['id'].'/votes' : '#' }}" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                             <img class="object-cover w-full rounded-t-lg h-120 md:h-60 md:w-20 md:rounded-none md:rounded-l-lg" src="/answer.jpeg" alt="">
                             <div class="flex flex-col justify-between p-4 leading-normal">
                                 <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{{ __('Answer with the most votes') }}</h5>
@@ -50,7 +50,7 @@
                         </a>
                     </div>
                     <div class="p-4 w-full">
-                        <a href="/questions/{{ $results?->highest_question['id'] }}/votes" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                        <a href="{{ $results?->highest_question['id'] ? '/questions/'.$results?->highest_question['id'].'/votes' : '#' }}" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                             <img class="object-cover w-full rounded-t-lg h-120 md:h-60 md:w-20 md:rounded-none md:rounded-l-lg" src="/questions.png" alt="">
                             <div class="flex flex-col justify-between p-4 leading-normal">
                                 <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{{ __('Question with the most answers')}}</h5>
@@ -60,7 +60,7 @@
                         </a>
                     </div>
                     <div class="p-4 w-full">
-                        <a href="/questions/{{ $results?->most_voted_question['id'] }}/votes" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                        <a href="{{ $results?->most_voted_question['id'] ? '/questions/'.$results?->most_voted_question['id'].'/votes' : '#' }}" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                             <img class="object-cover w-full rounded-t-lg h-120 md:h-60 md:w-20 md:rounded-none md:rounded-l-lg" src="/questions.png" alt="">
                             <div class="flex flex-col justify-between p-4 leading-normal">
                                 <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{{ __('Question with the highest number of votes') }}</h5>

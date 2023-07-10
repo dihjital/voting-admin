fe54376ea3a8cb970dc2b640b01439656e6fd4a9
dihjital <peter.hrobar@gmail.com>
2023-07-10 13:24:19 +0200

    Bug fixes and performance improvements

--- resources/views/livewire/show-questions.blade.php
+++ resources/views/livewire/show-questions.blade.php
@@ -25,7 +25,7 @@
                 <x-table.cell>
                     <label class="relative inline-flex items-center cursor-pointer">
                         <input type="checkbox" wire:click="openQuestion({{ $q['id'] }}, {{ $q['is_closed'] }})" value="" class="sr-only peer" {{ $q['is_closed'] ? 'checked' : '' }}>
-                        <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                        <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-indigo-300 dark:peer-focus:ring-indigo-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-indigo-600"></div>
                         <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
                     </label>
                 </x-table.cell>

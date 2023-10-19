98c2c50cade34e597dd17a30dcfc6545113eea02
dihjital <peter.hrobar@gmail.com>
2023-10-19 11:14:41 +0200

    Fix page footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,13 +1,14 @@
-<footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+<footer class="bg-gray-100 dark:bg-gray-900 m-4">
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div class="sm:flex sm:items-center sm:justify-between">
+        <div class="sm:flex sm:items-center">
             <!-- Logo -->
             <div class="shrink-0 flex items-center">
                 <a href="{{ route('dashboard') }}">
                     <x-application-mark class="block h-9 w-auto" />
                 </a>
             </div>
-            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            <span class="ml-4 self-center text-2xl font-semibold whitespace-nowrap text-gray-800 dark:text-gray-200">Where all votes matter</span>
+            <ul class="ml-auto flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
                     <a href="https://www.votes365.org" class="mr-4 hover:underline md:mr-6 ">About</a>
                 </li>
@@ -15,7 +16,7 @@
                     <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
                 </li>
                 <li>
-                    <a href="#" class="mr-4 hover:underline md:mr-6 ">Licensing</a>
+                    <a href="https://www.votes365.org/#pricing" class="mr-4 hover:underline md:mr-6 ">Licensing</a>
                 </li>
                 <li>
                     <a href="mailto:support@mg.votes365.org" class="hover:underline">Contact</a>

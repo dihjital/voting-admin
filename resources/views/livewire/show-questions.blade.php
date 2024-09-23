c4baa91f90fedaee52b80ce95d259d9fb3cf93ac
dihjital <peter.hrobar@gmail.com>
2024-09-23 18:18:52 +0200

    Conditional classes are added to correct_vote select input.

--- resources/views/livewire/show-questions.blade.php
+++ resources/views/livewire/show-questions.blade.php
@@ -192,27 +192,33 @@
             {{ __('If this question has a \'right\' answer then you can select it here. By default all answers are correct.') }}
             <div class="relative w-3/4 mt-4 mb-2" x-data="{}">
                 <div class="flex">
-                    <span 
-                        class="mt-1 px-3 py-2
-                            inline-flex items-center
-                            text-gray-500 dark:text-gray-400
-                            dark:bg-gray-700
-                            hover:text-white dark:hover:text-white 
-                            hover:bg-red-600 dark:hover:bg-red-600 
-                            border border-r-0 rounded-l-md 
-                            border-gray-300 dark:border-gray-700 hover:border-red-600 dark:hover:border-red-600"
-                        wire:click="$set('correct_vote', null)"
-                    >
-                        <i class="fas fa-trash" aria-hidden="true" title="{{ __('Delete current selection') }}"></i>
-                    </span>
+                    @if($correct_vote)
+                        <span 
+                            class="mt-1 px-3 py-2
+                                inline-flex items-center
+                                text-gray-500 dark:text-gray-400
+                                dark:bg-gray-700
+                                hover:text-white dark:hover:text-white 
+                                hover:bg-red-600 dark:hover:bg-red-600 
+                                border border-r-0 rounded-l-md 
+                                border-gray-300 dark:border-gray-700 hover:border-red-600 dark:hover:border-red-600"
+                            wire:click="$set('correct_vote', null)"
+                        >
+                            <i class="fas fa-trash" aria-hidden="true" title="{{ __('Delete current selection') }}"></i>
+                        </span>
+                    @endif
                     <select id="question-correct-vote-{{ $question_id }}"
                             wire:model.defer="correct_vote"
-                            class="mt-1 block w-full rounded-none rounded-r-md
-                                block flex-1 min-w-0
-                                border-gray-300 dark:border-gray-700 dark:bg-gray-900
-                                py-2 px-3 shadow-sm
-                                dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600
-                                sm:text-sm">
+                            @class([
+                                'mt-1 block w-full',
+                                'rounded-none rounded-r-md' => $correct_vote,
+                                'rounded-md' => ! $correct_vote,
+                                'block flex-1 min-w-0',
+                                'border-gray-300 dark:border-gray-700 dark:bg-gray-900',
+                                'py-2 px-3 shadow-sm',
+                                'dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600',
+                                'sm:text-sm',
+                            ])>
                             <option>{{ __('Please select') }}!</option>
                         @forelse($votes as $vote)
                             <option value="{{ $vote['id'] }}">

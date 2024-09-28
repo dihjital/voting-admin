1074d0b3db1d901fcc982d710ea2e803f87a131d
dihjital <peter.hrobar@gmail.com>
2024-09-28 14:47:48 +0200

    Protect /register api endpoints temporarily

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,9 @@ Route::get('/', function () {
     return redirect()->route('dashboard');
 });
 
+Route::get('/register', fn() => abort(404));
+Route::post('/register', fn() => abort(404));
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),

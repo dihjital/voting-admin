b1dcc2a45bd15bd4c2d116f161fc614a951301c3
peterhrtobar <peter.hrobar@gmail.com>
2025-03-27 09:52:33 +0100

    Docker configuration change

--- vite.config.js
+++ vite.config.js
@@ -18,9 +18,5 @@ export default defineConfig({
         host: '0.0.0.0',  // Allows access from outside the container
         port: 5173,       // Ensure it matches the Docker port
         strictPort: true,
-        hmr: {
-            host: 'localhost',
-            clientPort: 5173,
-        },
     },
 });
